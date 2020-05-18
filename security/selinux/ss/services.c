47e1adc132ff2156877c5653b4c47116ce80ddfd
Ian Macdonald <ian@caliban.org>
2020-05-18 10:07:32 +0200

    Import F907BXXU3BTDD source from SM-F907B_QQ_Opensource.zip.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -52,6 +52,12 @@
 #include <linux/selinux.h>
 #include <linux/flex_array.h>
 #include <linux/vmalloc.h>
+#ifdef CONFIG_UH
+#include <linux/uh.h>
+#ifdef CONFIG_RKP_KDP
+#include <linux/rkp.h>
+#endif
+#endif
 #include <net/netlabel.h>
 
 #include "flask.h"
@@ -91,7 +97,11 @@ static DEFINE_RWLOCK(policy_rwlock);
 
 static struct sidtab sidtab;
 struct policydb policydb;
+#if (defined CONFIG_RKP_KDP && defined CONFIG_SAMSUNG_PRODUCT_SHIP)
+int ss_initialized __kdp_ro;
+#else
 int ss_initialized __rticdata;
+#endif
 
 /*
  * The largest sequence number that has been used when
@@ -2110,7 +2120,11 @@ int security_load_policy(void *data, size_t len)
 		}
 
 		security_load_policycaps();
+#if (defined CONFIG_RKP_KDP && defined CONFIG_SAMSUNG_PRODUCT_SHIP)
+		uh_call(UH_APP_RKP, RKP_KDP_X60, (u64)&ss_initialized, 1, 0, 0);
+#else
 		ss_initialized = 1;
+#endif
 		seqno = ++latest_granting;
 		selinux_complete_init();
 		avc_ss_reset(seqno);
